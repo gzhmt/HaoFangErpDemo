752e6ca67b7d4a4239813738554c6825f1d990db
jdragon <1061917196@qq.com>
2020-03-23 22:19:07 +0800

    zhjl隐藏文件分支测试

--- src/main/java/com/jdragon/haoerpdemo/haofangerp/HaofangerpApplication.java
+++ src/main/java/com/jdragon/haoerpdemo/haofangerp/HaofangerpApplication.java
@@ -1,6 +1,5 @@
 package com.jdragon.haoerpdemo.haofangerp;
 
-import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
